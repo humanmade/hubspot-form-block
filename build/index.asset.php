26f1299c9a46778581ab496ce8382a5815101545
Samantha Miller <sam@humanmade.com>
2024-10-03 13:13:33 +0100

    Downgrade `wp-scripts` to v27
    v28 requires `react-jsx-runtime`, which is a dependency bundled in WP 6.6. This causes an error in 6.5 and below, and the block will not load. Downgrading to v27 uses `react` instead, available in both WP versions.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3334f83c26be9f0ba370');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b22ecd72e9e016cfe8d5');
